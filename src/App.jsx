16ea974a580c7bb3a71e7fc092deea0c4964ad42
Luis Henrique Ferreira da Silva <rm99465@estudante.fieb.edu.br>
2026-05-28 11:32:11 -0300

    home e menu funcionario em construção e criando o sistema de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 
-import './App.css'
+//import './App.css'
+import HomeFuncionario from './pages/HomeFuncionario/HomeFuncionario'
 
 function App() {
 
 
   return (
     <>
-      
+      <div className='container'>
+        <HomeFuncionario/>
+
+   
+      </div>
     </>
   )
 }
